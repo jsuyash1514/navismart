eb9d0e47363d64865c4c961a76019165743236a2
jsuyash1514 <jsuyash1514@gmail.com>
2018-12-12 12:37:49 +0530

    Add tootlbar in main activity

--- app/src/main/java/com/navismart/navismart/view/MainActivity.java
+++ app/src/main/java/com/navismart/navismart/view/MainActivity.java
@@ -15,7 +15,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
     }
 }
 
