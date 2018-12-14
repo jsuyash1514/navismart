dae71968e759ba1cba79f0f6224d07f39108b002
jsuyash1514 <jsuyash1514@gmail.com>
2018-12-14 20:27:07 +0530

    Add logout button

--- app/src/main/java/com/navismart/navismart/view/LandingFragment.java
+++ app/src/main/java/com/navismart/navismart/view/LandingFragment.java
@@ -5,10 +5,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.navismart.navismart.R;
 
+import androidx.navigation.NavOptions;
+import androidx.navigation.Navigation;
+
 public class LandingFragment extends Fragment {
+    FirebaseAuth auth;
     public LandingFragment() {
         // Required empty public constructor
     }
@@ -23,6 +30,27 @@ public class LandingFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_landing, container, false);
 
+        auth = FirebaseAuth.getInstance();
+        if(auth.getCurrentUser() == null){
+            Toast.makeText(getContext(), "No user found", Toast.LENGTH_SHORT).show();
+            NavOptions navOptions = new NavOptions.Builder()
+                    .setPopUpTo(R.id.landingFragment, true)
+                    .build();
+            Navigation.findNavController(getActivity(), R.id.my_nav_host_fragment).navigate(R.id.log_out_action, null, navOptions);
+        }
+
+        Button logout = view.findViewById(R.id.logout_button);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                auth.signOut();
+                Toast.makeText(getContext(), "Logged out Successful", Toast.LENGTH_SHORT).show();
+                NavOptions navOptions = new NavOptions.Builder()
+                        .setPopUpTo(R.id.landingFragment, true)
+                        .build();
+                Navigation.findNavController(getActivity(), R.id.my_nav_host_fragment).navigate(R.id.log_out_action, null,navOptions);
+            }
+        });
         return  view;
     }
 
