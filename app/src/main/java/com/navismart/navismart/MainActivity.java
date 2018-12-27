0cd04b7572c81571fb48b0d5921e151404917f90
Karthik Ramesh Iyer <Karthik.iyer2@yandex.com>
2018-12-27 19:04:18 +0530

    Implement PlacePicker during MarinaManager Signup

--- app/src/main/java/com/navismart/navismart/MainActivity.java
+++ app/src/main/java/com/navismart/navismart/MainActivity.java
@@ -1,12 +1,19 @@
 package com.navismart.navismart;
 
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
+import com.google.android.gms.location.places.GeoDataClient;
+import com.google.android.gms.location.places.PlaceDetectionClient;
+import com.google.android.gms.location.places.Places;
+
 public class MainActivity extends AppCompatActivity {
 
     public static Toolbar toolbar;
+    protected GeoDataClient mGeoDataClient;
+    protected PlaceDetectionClient mPlaceDetectionClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +23,9 @@ public class MainActivity extends AppCompatActivity {
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
+        mGeoDataClient = Places.getGeoDataClient(this, null);
+        mPlaceDetectionClient = Places.getPlaceDetectionClient(this, null);
     }
+
 }
 
