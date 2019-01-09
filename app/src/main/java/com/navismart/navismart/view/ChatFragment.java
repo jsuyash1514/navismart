6f014659f6e949a8ae3914d41133ead4c700974d
Karthik Ramesh Iyer <Karthik.iyer2@yandex.com>
2019-01-09 19:30:37 +0530

    Create chat UI

---
+++ app/src/main/java/com/navismart/navismart/view/ChatFragment.java
@@ -0,0 +1,30 @@
+package com.navismart.navismart.view;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.navismart.navismart.R;
+
+public class ChatFragment extends Fragment {
+
+    public ChatFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_chat, container, false);
+        return view;
+    }
+
+}
